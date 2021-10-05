f22968e8462d43aaa8e28e859560a30c494a457b
jag3921 <jag3921@rit.edu>
2021-10-05 18:27:09 -0400

    updated app page

--- src/responses.js
+++ src/responses.js
@@ -4,16 +4,8 @@ let data = fs.readFileSync('./src/trivia.json','utf8');
 
 const getResponse = (request, response, params, acceptedTypes, httpMethod) => { //eslint-disable-line
     response.writeHead(200, {'Content-Type': 'text/json'});
-    console.log(params.selection);
-    let obj = JSON.parse(data);
-    console.log(obj);
-   // let responseData;
-    for (let index in obj) {
-        console.log(`${index} : ${obj[index]}`);
-    }
     response.write(data);
     response.end();
 }
-
 
 module.exports.getResponse = getResponse;
